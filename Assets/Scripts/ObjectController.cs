4ab84fe162b8a0c2424d939279c76924fd4d6a57
kimkisna <90582361+kimkisna@users.noreply.github.com>
2024-03-05 23:27:33 +0700

    Inventory_Update

---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObjectController : MonoBehaviour
+{
+    public void Object1Btn()
+    {
+        Debug.Log("Game");
+        SceneManager.LoadScene("MainGame");
+
+    }
+
+    public void Object2Btn()
+    {
+        Debug.Log("Game");
+        SceneManager.LoadScene("MainGame");
+
+    }
+    public void Object3Btn()
+    {
+        Debug.Log("Game");
+        SceneManager.LoadScene("MainGame");
+
+    }
+}
